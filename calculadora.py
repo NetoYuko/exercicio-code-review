db9afd4dd44f2aed10b0a9c06bb1b8229d055b04
Aluizio Antônio <neto.yuko197@gmail.com>
2026-09-08 20:44:11 -0300

    chore: adiciona calculadora base

---
+++ calculadora.py
@@ -0,0 +1,15 @@
+def calc(a, b, op): 
+    if op == "soma": 
+        return a + b 
+    elif op == "sub": 
+        return a - b 
+    elif op == "mult": 
+        return a * b 
+    elif op == "div": 
+        return a / b 
+
+if __name__ == "__main__": 
+    print(calc(10, 5, "soma")) 
+    print(calc(10, 5, "sub")) 
+    print(calc(10, 5, "mult")) 
+    print(calc(10, 5, "div"))

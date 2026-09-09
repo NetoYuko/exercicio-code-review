cd341c351c0a038ef5b4682847408c46985398ea
Tonidev <antoniodev2709@gmail.com>
2026-09-08 21:40:20 -0300

    feat: adiciona operação de raiz quadrada

--- calculadora.py
+++ calculadora.py
@@ -7,9 +7,12 @@ def calc(a, b, op):
         return a * b 
     elif op == "div": 
         return a / b 
+    elif op == "raiz":
+        return a ** 0.5
 
 if __name__ == "__main__": 
     print(calc(10, 5, "soma")) 
     print(calc(10, 5, "sub")) 
     print(calc(10, 5, "mult")) 
     print(calc(10, 5, "div"))
+    print(calc(10, 5, "raiz"))

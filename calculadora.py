2261819ad693e2067013fcda3ebd1613d5c3170b
Aluizio Antônio <neto.yuko197@gmail.com>
2026-09-08 21:06:58 -0300

    feat: adiciona operação de potenciação

--- calculadora.py
+++ calculadora.py
@@ -7,9 +7,12 @@ def calc(a, b, op):
         return a * b 
     elif op == "div": 
         return a / b 
+    elif op == "pot":
+        return a ** b
 
 if __name__ == "__main__": 
     print(calc(10, 5, "soma")) 
     print(calc(10, 5, "sub")) 
     print(calc(10, 5, "mult")) 
     print(calc(10, 5, "div"))
+    print(calc(2, 3, "pot"))
